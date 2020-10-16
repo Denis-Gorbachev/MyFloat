d0142383337d2ad0de2cb82f3ed643ad723cab5e
Denis-Gorbachev <72994243+Denis-Gorbachev@users.noreply.github.com>
2020-10-16 22:32:53 +0300

    Update Main.java

--- MyFloat/Main.java
+++ MyFloat/Main.java
@@ -1,10 +1,10 @@
+package com.company;
 public class Main 
 {
     public static void main (String[] args)
     {
         MyFloat a = new MyFloat(12343445, 6);
         MyFloat b = new MyFloat(8656645, 4);
-
         a.sub(b);
     }
 }
